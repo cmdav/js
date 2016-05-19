03e0b7ae613ff5586eedac0b583adad50d02ea5f
Davies Christopher <davincin222@gmail.com>
2016-05-19 08:48:00 +0100

    This version finally got the test of get and create function working

--- notesapplication.js
+++ notesapplication.js
@@ -1,47 +1,60 @@
-var NotesApplication = function(author) {
+var NotesApplication = function(auth) {
 
-this.author = author;
-
-this.listNotes = new Array();
+ this.author = auth;
+//this.listNotes = function() 
+ 
 };
-
+this.list = [];
 //this.create = function(note_content);
-//this.listNotes = function() ;
 
-NotesApplication.prototype.create = function(note_content) 
-	{ 
-		listNotes.push(note_content);
-		
-	};
 NotesApplication.prototype.listNotes = function() 
 	{ 
-		for (var i=0;i<length(listNotes);i++) 
+		for (var i=0;i<list.length;i++) 
 				{
-					console.log("Note ID: " + " "+ indexOf(listNotes(i)) + "\n" + "By Author " + listNotes(i) );
+					console.log("Note ID: " + " "+ indexOf(list(i)) + "\n" + "By Author " + list(i) );
 							
 				} 
 	};
+
+NotesApplication.prototype.create = function(note_content) 
+	{ 	
+				list.push(note_content);
+	};
+
 NotesApplication.prototype.get = function(note_id)
 	{	if (isNaN(note_id)) { console.log("Please ensure you input the right index value");}
 		else {
-			console.log(" The note for ID " + noteid + " is " + listNotes(noteid));
+			console.log(" The note for ID " + note_id + " is " + list[note_id]);
 			}			
 	};
 NotesApplication.prototype.search = function(search_text)
 	{ 
 		console.log("Showing results for search " + search_text + "\n");
-		console.log("Note ID: " + " "+ indexOf(listNotes.match(/search_text/gi)) + "\n" + listNotes.match(/search_text/gi)+"\n"+ "By Author " + this.author);
+		console.log("Note ID: " + " "+ indexOf(list.match(/search_text/gi)) + "\n" + list.match(/search_text/gi)+"\n"+ "By Author " + this.author);
 	};
 							   
 NotesApplication.prototype.delete = function(note_id)
 	{
-		delete listNotes(note_id);	
+		delete list(note_id);	
 	};
 
 NotesApplication.prototype.edit = function(note_id, new_content)
 { 
-	listNodes.replace(note_id,new_content);
+	list.replace(note_id,new_content);
 };
 
-var notes1 = new NotesApplication("Davies");
+var notes1 = new NotesApplication('Davies');
+var notes2 = new NotesApplication();
 console.log("The Author is " + notes1.author);
+notes2.create('Mongidi');
+notes2.create('Suberu');
+notes2.create('Risikatu');
+notes2.search('i');
+notes2.get(1);
+
+for (i=0;i<list.length;i++)
+{
+console.log(this.list[i] + "\n");
+}
+
+
